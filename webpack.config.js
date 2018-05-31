75c23baadd09803aba7c97f65fa79c123acdfed0
Eric Amodio <eamodio@gmail.com>
2018-05-31 01:32:43 -0400

    Updates dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 'use strict';
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const WriteFilePlugin = require('write-file-webpack-plugin');
 
@@ -12,21 +12,23 @@ module.exports = function(env, argv) {
         env = {};
     }
 
-    const minify = !!env.production;
+    const production = !!env.production;
+
+    const minify = production;
     const prefixCss = true;
-    const sourceMaps = !env.production;
+    const sourceMaps = !production;
 
     const plugins = [
         new WriteFilePlugin(),
         new webpack.optimize.ModuleConcatenationPlugin(),
-        new ExtractTextPlugin({
+        new MiniCssExtractPlugin({
             filename: 'main.css'
         }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             filename: path.resolve(__dirname, 'index.html'),
             inject: true,
-            inlineSource: env.production ? '.(js|css)$' : undefined,
+            inlineSource: production ? '.(js|css)$' : undefined,
             minify: minify
                 ? {
                     removeComments: true,
@@ -51,18 +53,30 @@ module.exports = function(env, argv) {
                     beautify: !minify,
                     comments: false,
                     ecma: 5
-                },
-                sourceMap: sourceMaps,
+                }
             }
         })
     ];
 
     return {
         entry: [ './src/index.ts', './src/scss/main.scss' ],
+        mode: production ? 'production' : 'development',
         output: {
             filename: '[name].js',
             path: path.resolve(__dirname, 'dist'),
             publicPath: '/dist/'
+        },
+        optimization: {
+            splitChunks: {
+              cacheGroups: {
+                styles: {
+                  name: 'styles',
+                  test: /\.css$/,
+                  chunks: 'all',
+                  enforce: true
+                }
+              }
+            }
         },
         resolve: {
             extensions: ['.ts', '.js'],
@@ -71,7 +85,7 @@ module.exports = function(env, argv) {
         externals: {
             'typed.js': 'Typed'
         },
-        devtool: sourceMaps ? 'inline-source-map' : false,
+        devtool: sourceMaps ? 'eval-source-map' : false,
         module: {
             rules: [
                 {
@@ -81,35 +95,35 @@ module.exports = function(env, argv) {
                 },
                 {
                     test: /\.scss$/,
-                    use: ExtractTextPlugin.extract({
-                        fallback: 'style-loader',
-                        use: [
-                            {
-                                loader: 'css-loader',
-                                options: {
-                                    minimize: minify,
-                                    sourceMap: sourceMaps,
-                                    url: false
-                                }
-                            },
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    ident: 'postcss',
-                                    plugins: prefixCss
-                                        ? [ require('autoprefixer')({ browsers: ['last 2 versions'] }) ]
-                                        : [],
-                                    sourceMap: sourceMaps
-                                }
-                            },
-                            {
-                                loader: 'sass-loader',
-                                options: {
-                                    sourceMap: sourceMaps
-                                }
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: minify,
+                                sourceMap: sourceMaps,
+                                url: false
                             }
-                        ]
-                    }),
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: prefixCss
+                                    ? [ require('autoprefixer')({ browsers: ['last 2 versions'] }) ]
+                                    : [],
+                                sourceMap: sourceMaps
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: sourceMaps
+                            }
+                        }
+                    ],
                     exclude: /node_modules/
                 }
             ]
